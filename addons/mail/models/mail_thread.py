a921234067fadfb8a3968731db07643620a807c4
dbertha <david.bertha@gmail.com>
2021-08-13 09:18:36 +0200

    ensure no bounce if direct contact of catchall

--- addons/mail/models/mail_thread.py
+++ addons/mail/models/mail_thread.py
@@ -995,10 +995,10 @@ class MailThread(models.AbstractModel):
             # check it does not directly contact catchall
             if catchall_alias and email_to_localparts and all(email_localpart == catchall_alias for email_localpart in email_to_localparts):
                 _logger.info('Routing mail from %s to %s with Message-Id %s: direct write to catchall, bounce', email_from, email_to, message_id)
-                body = self.env.ref('mail.mail_bounce_catchall').render({
-                    'message': message,
-                }, engine='ir.qweb')
-                self._routing_create_bounce_email(email_from, body, message, reply_to=self.env.company.email)
+                # body = self.env.ref('mail.mail_bounce_catchall').render({
+                #     'message': message,
+                # }, engine='ir.qweb')
+                # self._routing_create_bounce_email(email_from, body, message, reply_to=self.env.company.email)
                 return []
 
             dest_aliases = self.env['mail.alias'].search([('alias_name', 'in', rcpt_tos_valid_localparts)])
